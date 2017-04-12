c6cfdf1b08f2aee7efb9be30e8f4ea922b3c8d03
Carmen <Student@10.0.2.15>
2017-04-12 10:24:13 +0200

    Test funcionales

---
+++ Acme-Chorbies/src/test/java/useCases/QuitLikeAnotherChorbieTest.java
@@ -0,0 +1,86 @@
+package useCases;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import services.AdministratorService;
+import services.ChorbiService;
+import utilities.AbstractTest;
+import domain.Administrator;
+import domain.Chorbi;
+
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class QuitLikeAnotherChorbieTest extends AbstractTest{
+	
+	/* *----Browse the list of chorbies who have registered to the system.-----*
+	  -El orden de los parámetros es: Usuario que se va a autenticar, error esperado
+	  
+	  Cobertura del test:
+	  		//El usuario autenticado existe(test positivo)
+			//El usuario no está autenticado(test negativo)
+				
+	 */
+	
+	@Autowired
+	private ChorbiService chorbiService;
+	
+	@Autowired
+	private AdministratorService adminService;
+	
+	private List<Chorbi> chorbies;
+	
+	@Before
+    public void setup() {
+		this.chorbies= new ArrayList<Chorbi>();
+		this.chorbies.addAll(this.chorbiService.findAll());
+		
+		Collections.shuffle(this.chorbies);
+	}
+	
+	@Test
+	public void driver() {
+		final Object testingData[][] = {
+			{
+				this.chorbies.get(0).getUserAccount().getUsername(), null
+			}, {
+				"", IllegalArgumentException.class
+			},
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.template((String) testingData[i][0], (Class<?>) testingData[i][1]);
+	}
+	
+
+	protected void template(final String username, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
+		try {
+			this.authenticate(username);
+			
+			chorbiService.findMyLikes(likedId)
+			List<Chorbi> chorbies = new ArrayList<Chorbi>();
+			chorbies.addAll(chorbiService.findAll());
+			
+			this.unauthenticate();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+		this.checkExceptions(expected, caught);
+	}
+
+}
