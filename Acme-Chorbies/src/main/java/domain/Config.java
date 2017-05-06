84e4ddd4df76e772d507898efd39129486180a16
jospalhid <Es25Alba>
2017-05-06 21:38:20 +0200

    Config

--- Acme-Chorbies/src/main/java/domain/Config.java
+++ Acme-Chorbies/src/main/java/domain/Config.java
@@ -3,6 +3,9 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.validation.constraints.Min;
+
+import org.hibernate.validator.constraints.Range;
 
 @Entity
 @Access(AccessType.PROPERTY)
@@ -13,22 +16,23 @@ public class Config extends DomainEntity{
 	private double fee;
 	private double registrationFee;
 
+	@Range(min=0, max=10000)
 	public int getCache() {
 		return cache;
 	}
-
 	public void setCache(int cache) {
 		this.cache = cache;
 	}
 
+	@Min(0)
 	public double getFee() {
 		return fee;
 	}
-
 	public void setFee(double fee) {
 		this.fee = fee;
 	}
 
+	@Min(0)
 	public double getRegistrationFee() {
 		return registrationFee;
 	}
